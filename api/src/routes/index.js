2c2f93fc68f66d57fcb147e2ee3b51b993b589e7
IronHack <guille.fernandeeez@gmail.com>
2022-10-31 03:14:10 -0300

    qq

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -28,7 +28,6 @@ router.put('/product/:id_product', putProductById)
 router.post('/product/images', upload.array("imagen", 5), addImagesByIdProduct)
 router.post('/product/image', upload.single("imagen1"), addImagesByIdProductSingle)
 //rutas Users
-
 router.get('/users', verification, getUsers)
 router.post('/users', postUsers)
 router.put('/verification/:id', putUserById1)
