05bad1a64d723ed1d2ccd293476dfc9a9c857948
MarcosHeick <marcos.heick@gmail.com>
2022-11-02 10:52:05 -0300

    asd

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -18,7 +18,27 @@ const path = require('path');
 const { getFavorites,addFavorite } = require('../controllers/Favs');
 
 const router = Router();
-
+const allowCors = fn => async (req, res) => {
+    res.setHeader('Access-Control-Allow-Credentials', true)
+    res.setHeader('Access-Control-Allow-Origin', '*')
+    // another common pattern
+    // res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
+    res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT')
+    res.setHeader(
+      'Access-Control-Allow-Headers',
+      'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
+    )
+    if (req.method === 'OPTIONS') {
+      res.status(200).end()
+      return
+    }
+    return await fn(req, res)
+  }
+  
+  const handler = (req, res) => {
+    const d = new Date()
+    res.end(d.toString())
+  }
 // Configurar los routers
 // Ejemplo: router.use('/auth', authRouter);
 router.post('/product', plusProduct)
@@ -30,7 +50,7 @@ router.post('/product/images', upload.array("imagen", 5), addImagesByIdProduct)
 router.post('/product/image', upload.single("imagen1"), addImagesByIdProductSingle)
 //rutas Users
 router.get('/users', verification, getUsers)
-router.post('/users', postUsers)
+router.post('/users',allowCors, postUsers)
 router.put('/verification/:id', putUserById1)
 router.put('/users/:id', putUserById)
 
