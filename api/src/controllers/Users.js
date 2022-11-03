d8f589f88b40dde266790cd37ec4e4725f413ae7
carlos pana <panacarlosramon@gmail.com>
2022-11-03 10:39:15 -0300

    puttttt

--- api/src/controllers/Users.js
+++ api/src/controllers/Users.js
@@ -1,7 +1,7 @@
 
 
 const bcryptjs = require('bcryptjs')
-const { User,Favorite,UserFav,Order, OrderProduct} = require('../db')
+const { User, Favorite, UserFav, Order, OrderProduct } = require('../db')
 const jwt = require('jsonwebtoken');
 const express = require('express');
 const { sendEmail } = require('./SendEmail');
@@ -22,27 +22,27 @@ const as = () => {
 const allUsers = async function () {
 
 
-    const a= await User.findAll({include:[{ model: Favorite} ]})
+    const a = await User.findAll({ include: [{ model: Favorite }] })
 
 
-return a
+    return a
 
 
 
-//    const b = await a?.map(er=>{
-//     return {
-//         id: er.id,
-//         userName:er.userName,
-//         email:er.email,
-//         password:er.password,
-//         image:er.image ,
-//         phoneNumber: er.phoneNumber,
-//         role:er.role ,
-//         random:er.random,
-//         favorites: er.favorites?.map(el=> {return{idProduct: el.idProduct, verify :el.userFav?.verify}})
-//     }
-// }
-   //)
+    //    const b = await a?.map(er=>{
+    //     return {
+    //         id: er.id,
+    //         userName:er.userName,
+    //         email:er.email,
+    //         password:er.password,
+    //         image:er.image ,
+    //         phoneNumber: er.phoneNumber,
+    //         role:er.role ,
+    //         random:er.random,
+    //         favorites: er.favorites?.map(el=> {return{idProduct: el.idProduct, verify :el.userFav?.verify}})
+    //     }
+    // }
+    //)
 
 
 }
@@ -52,13 +52,13 @@ const getUsers = async function (req, res) {
     try {
         let a = await allUsers()
         //console.log(a)
-        
+
         return res.status(200).send(a)
     } catch (error) {
 
         return res.status(400).json({ error: error.message })
     }
-    
+
 }
 
 const postUsers = async function (req, res) {
@@ -73,72 +73,74 @@ const postUsers = async function (req, res) {
         googleId,
     } = req.body
     console.log(password)
-     let pas = await bcryptjs.hash(password, 8)
- //console.log(pas)
- if(googleId===undefined){
-   googleId= false
- }
-
-if (googleId===false){
-    let a = await allUsers();
-    //console.log("esto es a ", a)
-    
-    
-    let b = a.filter(e => e.userName === userName)
-    let c = a.filter(o => o.email === email)
-
-
-
-     if(c[0]){
-
-         return res.send('email ya registrado')
-     }
-    
-     if(b[0]){
-
-       return res.status(200).send('ya tenemos creado ese usuario, prueba con otro')
-
+    let pas = await bcryptjs.hash(password, 8)
+    //console.log(pas)
+    if (googleId === undefined) {
+        googleId = false
     }
- const random = as()
-    try {
-        let userCreated = await User.create({
-            userName,
-            password: pas,
-            email,
-            image,
-            phoneNumber,
-            role,
-            random
-        })
-      //  console.log(userCreated.dataValues)
 
-        const ID = userCreated.id
-        await sendEmail(email, ID,random)
+    if (googleId === false) {
+        let a = await allUsers();
+        //console.log("esto es a ", a)
 
-        res.send(await postLogin(req,res))
-    } catch (error) {
-        return res.status(400).json({ error: error.message })
-} 
-}
-    else{
 
-   const random = as()
-    try {
-        let userCreated = await User.findOrCreate({ where: { userName :userName},
-           defaults:{  
-            password: pas,
-           email: email,
-           image,
-           phoneNumber,
-           role: "active",
-           random: random}
-        })
-   
+        let b = a.filter(e => e.userName === userName)
+        let c = a.filter(o => o.email === email)
 
-        res.send(await postLogin(req,res))
-    } catch (error) {
-        return res.status(400).json({ error: error.message })
+
+
+        if (c[0]) {
+
+            return res.send('email ya registrado')
+        }
+
+        if (b[0]) {
+
+            return res.status(200).send('ya tenemos creado ese usuario, prueba con otro')
+
+        }
+        const random = as()
+        try {
+            let userCreated = await User.create({
+                userName,
+                password: pas,
+                email,
+                image,
+                phoneNumber,
+                role,
+                random
+            })
+            //  console.log(userCreated.dataValues)
+
+            const ID = userCreated.id
+            await sendEmail(email, ID, random)
+
+            res.send(await postLogin(req, res))
+        } catch (error) {
+            return res.status(400).json({ error: error.message })
+        }
     }
+    else {
+
+        const random = as()
+        try {
+            let userCreated = await User.findOrCreate({
+                where: { userName: userName },
+                defaults: {
+                    password: pas,
+                    email: email,
+                    image,
+                    phoneNumber,
+                    role: "active",
+                    random: random
+                }
+            })
+
+
+            res.send(await postLogin(req, res))
+        } catch (error) {
+            return res.status(400).json({ error: error.message })
+        }
     }
 }
 const putUserById = async (req, res) => {
@@ -147,11 +149,11 @@ const putUserById = async (req, res) => {
     console.log("destructurados", email, image, phoneNumber, userName)
     let arr = {}
     if (email) arr.email = email
-    if (image) arr.image = image     
-        /* let i = image.slice(12, image.length)
-        console.log(i)
-        arr.image = i */
-    
+    if (image) arr.image = image
+    /* let i = image.slice(12, image.length)
+    console.log(i)
+    arr.image = i */
+
     if (phoneNumber) arr.phoneNumber = phoneNumber
     if (userName) arr.userName = userName
     const { id } = req.params;
@@ -170,17 +172,17 @@ const putUserById = async (req, res) => {
                 upUser.email = req.body.email;
                 break;
             case 'image':
-                upUser.image = req.body.image; 
+                upUser.image = req.body.image;
                 break;
             case 'phoneNumber':
                 upUser.phoneNumber = req.body.phoneNumber;
                 break;
-            /*  case 'address':
-                 upUser.address = req.body.address;
-                 break;
-             case 'role':
-                 upUser.role = req.body.role;
-                 break; */
+            // case 'address':
+            //      upUser.address = req.body.address;
+            //      break;
+            case 'role':
+                upUser.role = req.body.role;
+                break;
         }
     });
 
@@ -207,33 +209,33 @@ const putUserById1 = async (req, res) => {
     console.log(bringUser.dataValues.role)
     if (bringUser.dataValues.random === random) {
 
-    const {random} = req.body
-    //console.log("2123")
-    const bringUser = await User.findByPk(id, {});
-   // console.log(bringUser.dataValues.role)
-    if (bringUser.dataValues.random === random){
+        const { random } = req.body
+        //console.log("2123")
+        const bringUser = await User.findByPk(id, {});
+        // console.log(bringUser.dataValues.role)
+        if (bringUser.dataValues.random === random) {
 
 
-        let upUser = {};
+            let upUser = {};
 
-        upUser.role = "active";
+            upUser.role = "active";
 
 
 
-        try {
-            await User.update(upUser, { where: { id } })
-            return res.status(200).json('updated information!!');
-        } catch (error) {
-            return res.status(400).json({ error: error.message })
+            try {
+                await User.update(upUser, { where: { id } })
+                return res.status(200).json('updated information!!');
+            } catch (error) {
+                return res.status(400).json({ error: error.message })
+            }
         }
-    }
-    else {
-        return res.status(404).json('Wrong Number verification')
-    }
+        else {
+            return res.status(404).json('Wrong Number verification')
+        }
 
-    // const fetchUsers = await User.findByPk(id_user,{})
+        // const fetchUsers = await User.findByPk(id_user,{})
 
-}
+    }
 }
 
 const addOrder = async function (req, res) {
@@ -288,35 +290,35 @@ const addOrder = async function (req, res) {
 
 
 const postLogin = async function (req, res) {
-    
-    const {userName, password} = req.body
+
+    const { userName, password } = req.body
 
     const Users = await allUsers();
 
-    const a = Users.filter( e => e.userName === userName)
-    console.log("hola",a[0].dataValues.password)
+    const a = Users.filter(e => e.userName === userName)
+    console.log("hola", a[0].dataValues.password)
     //console.log(a.length)
     let pas = await bcryptjs.compare(password, a[0].dataValues?.password)
-    console.log("esto es pas ",pas)
-  try{
-    if(/* a.length && a[0].dataValues?.password === password */pas){
-        const payload = {
-            check:true
+    console.log("esto es pas ", pas)
+    try {
+        if (/* a.length && a[0].dataValues?.password === password */pas) {
+            const payload = {
+                check: true
+            }
+            const token = jwt.sign(payload, keys.key, {
+                expiresIn: '1d'
+            })
+            console.log([a[0], token])
+            res.json([a[0], token])
+        } else {
+            res.json({
+                menssage: 'Usuario y/o password son incorrectos'
+            })
         }
-       const token = jwt.sign(payload,keys.key ,{
-            expiresIn:'1d'
-         })
-        console.log([a[0] , token  ])
-        res.json( [a[0] , token  ])
-    }else{
-        res.json({
-            menssage:'Usuario y/o password son incorrectos'
-        })
+
     }
-    
-    }
-    catch(error){
+    catch (error) {
         res.status(200).send(json({ error: error.message }))
     }
 }
-module.exports = { getUsers, postUsers, putUserById, allUsers, putUserById1 , addOrder, postLogin};
+module.exports = { getUsers, postUsers, putUserById, allUsers, putUserById1, addOrder, postLogin };
